130f627680c3ffc2b9b0d7b3a7375c7f69ed1220
Niroshan Sugirtharatnam <niro.junk@gmail.com>
2014-06-23 17:28:18 -0400

    Initial Commit... + started Local Queue (non-storage) API

---
+++ index.js
@@ -0,0 +1,9 @@
+/*!
+ * Chronus
+ * Copyright(c) 2014 Niroshan Sugirtharatnam (GeekMode). All Rights Reserved.
+ * MIT Licensed
+ */
+
+"use strict";
+
+module.exports = require('./lib/chronus.js');
---
+++ test/lib/chronus.test.js
@@ -0,0 +1,45 @@
+/*!
+ * Chronus
+ * Copyright(c) 2014 Niroshan Sugirtharatnam (GeekMode). All Rights Reserved.
+ * MIT Licensed
+ *
+ *
+ * Main API
+ */
+
+var should = require('should'),
+    chronus = require('../../lib/chronus.js');
+
+describe("Chronus -", function() {
+    /*
+        Version & Tool Info
+     */
+    it("#name & #version return valid data", function() {
+        chronus.name.should.be.exactly('Chronus');
+        chronus.version.should.be.exactly('0.0.1');
+    });
+
+
+    describe("constructor for local queues", function() {
+        it("#createLocalQueue creates Local Message Queue", function() {
+            var queue = chronus.createLocalQueue();
+        });
+
+        it("#createLocalQueue returns a queue object", function() {
+            var localQueue = chronus.createLocalQueue();
+            (typeof localQueue).should.equal('object');
+
+            /*
+                Tests for the returned object should be performed in its ow
+             */
+        });
+
+        it("#createLocalQueue takes ", function() {
+
+        })
+
+
+    })
+});
+
+
